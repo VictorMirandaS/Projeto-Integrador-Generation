dae62b1141d47e9df15cea9829ed3bb32cad8aae
lingonsylt <rafael.rodrigues@tuta.io>
2020-11-04 09:25:54 -0300

    Uploanding spring boot bootstrap

---
+++ Agreeks/src/test/java/com/AgreeingUp/Agreeks/AgreeksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AgreeingUp.Agreeks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgreeksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
