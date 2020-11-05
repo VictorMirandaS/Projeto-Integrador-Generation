b5c1ed58f6a73a24e21c84487547f60542706489
Bianca Golin <biancagolin@hotmail.com>
2020-11-05 14:48:37 -0300

    criacao do pacote model, da classe categoria, seus atributos, metodos e notacoes

---
+++ Agreeks/src/main/java/com/AgreeingUp/Agreeks/model/Categoria.java
@@ -0,0 +1,36 @@
+package com.AgreeingUp.Agreeks.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import com.sun.istack.NotNull;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+@Entity
+@Table(name = "tb_tema")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@NotNull
+	@Size(min = 3, max = 255)
+	private String nomeCategoria;
+
+	@NotNull
+	@Size(min = 5, max = 2500)
+	private String descricao;
+
+	@NotNull
+	private int proposito;
+
+}
